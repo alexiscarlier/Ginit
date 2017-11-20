149649c798abb5eb5b9245835f98bd74165a421c
Alexis <alexis@Alexiss-MacBook-Pro.local>
2017-11-20 16:00:14 +0000

    Setup and file structure, printed GINIT to the command line

---
+++ testing/index.js
@@ -0,0 +1,22 @@
+var files = require('./lib/files');
+
+var chalk       = require('chalk');
+var clear       = require('clear');
+var CLI         = require('clui');
+var figlet      = require('figlet');
+var inquirer    = require('inquirer');
+var Preferences = require('preferences');
+var Spinner     = CLI.Spinner;
+var GitHubApi   = require('github');
+var _           = require('lodash');
+var git         = require('simple-git')();
+var touch       = require('touch');
+var fs          = require('fs');
+
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('GINIT', { horizontalLayout: 'full' })
+  )
+);
